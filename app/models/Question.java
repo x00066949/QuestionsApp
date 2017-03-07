1f0590be7c24ddb8004bd0c10e13452cb8315f4a
Mariam Nuga <x00066949@gmail.com>
2017-03-07 12:21:40 +0000

    First commit

---
+++ app/models/Question.java
@@ -0,0 +1,17 @@
+package models;
+
+import javax.persistennce.*;
+
+@Entity
+ /**
+ * Question class to hold a question object
+ */
+public class Question {
+
+    String question; 
+    public Question (string question) {
+        this.question=question;
+    }
+
+    
+}
